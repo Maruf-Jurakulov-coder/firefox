0788f305995558d4b331b41acb35a6059d6442c9
Maruf-Jurakulov-coder <71251828+Maruf-Jurakulov-coder@users.noreply.github.com>
2021-03-16 09:43:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from instapy import InstaPy
 
-session = InstaPy(username="mabimin452", password="9467151m")
+session = InstaPy(username="mabimin452", password="9467151m", headless_browser=True)
 session.login()
 session.like_by_tags(["bmw", "mercedes"], amount=5)
 session.set_dont_like(["naked", "nsfw"])
